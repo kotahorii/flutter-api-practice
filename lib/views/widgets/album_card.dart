2c70c92eb2166070e5186aaa1d9b846a48c9a04c
kotahorii <khorii.micron@gmail.com>
2022-02-04 10:02:10 +0900

    [Update] card style

--- lib/views/widgets/album_card.dart
+++ lib/views/widgets/album_card.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 class AlbumCard extends StatelessWidget {
-  const AlbumCard({Key? key}) : super(key: key);
+  final String imageUrl;
+  final int likes;
+  final String user;
+
+  const AlbumCard(
+      {Key? key,
+      required this.imageUrl,
+      required this.likes,
+      required this.user})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ,
+      margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+      height: 180,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.6),
+              offset: const Offset(0.0, 10.0),
+              blurRadius: 10.0,
+              spreadRadius: -6.0)
+        ],
+        image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.35), BlendMode.multiply),
+            image: NetworkImage(imageUrl),
+            fit: BoxFit.cover),
+      ),
+      child: Stack(
+        children: [
+          
+        ],
+      ),
     );
   }
 }
